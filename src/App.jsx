fd40e9d14e5e6de83ac70e856c0e7f04d3796116
Umut Keskin <ukeskin2626@gmail.com>
2025-02-20 14:55:13 +0300

    disable pwa

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,21 @@
 import { Toaster } from "@egaranti/components";
 
 import { useEffect } from "react";
-import { useTranslation } from "react-i18next";
 import { RouterProvider } from "react-router-dom";
 
 import routes from "./routes";
 
 import { getUserCountry } from "@/services/country";
 
-import { useRegisterSW } from "virtual:pwa-register/react";
-
 const App = () => {
-  // 15 minutes
-  const intervalMS = 900000;
-
-  const {
-    offlineReady: [offlineReady, setOfflineReady],
-    needRefresh: [needRefresh, setNeedRefresh],
-    updateServiceWorker,
-  } = useRegisterSW({
-    onRegisteredSW(swUrl, r) {
-      if (!r) return;
-
-      setInterval(async () => {
-        try {
-          if (!navigator.onLine) return;
-
-          const response = await fetch(swUrl, {
-            cache: "no-store",
-            headers: { "cache-control": "no-cache" },
-          });
-
-          if (response?.status === 200) {
-            await r.update();
-            setNeedRefresh(true);
-          }
-        } catch (error) {
-          console.error("SW Update Error:", error);
-        }
-      }, intervalMS);
-    },
-    onRegistered(r) {
-      // eslint-disable-next-line prefer-template
-      console.log("SW Registered: " + r);
-    },
-    onRegisterError(error) {
-      console.log("SW registration error", error);
-    },
-  });
-
-  const close = () => {
-    setOfflineReady(false);
-    setNeedRefresh(false);
-  };
-
   useEffect(() => {
     getUserCountry();
   }, []);
-
-  const { t } = useTranslation();
 
   return (
     <>
       <Toaster />
       <RouterProvider router={routes} />
-      <div className="fixed bottom-4 right-4">
-        {needRefresh && (
-          <div className="flex items-center space-x-4 rounded-lg bg-white p-4 shadow-lg">
-            <div className="flex-1">
-              <span>
-                {t(
-                  "needReflesh.reload",
-                  "Yeni içerik mevcut, güncellemek için yeniden yükle düğmesine tıklayın.",
-                )}
-              </span>
-            </div>
-            {needRefresh && (
-              <button
-                className="rounded-lg bg-blue-600 px-4 py-2 text-white"
-                onClick={() => {
-                  updateServiceWorker(true);
-                  close();
-                  window.location.reload(true);
-                }}
-              >
-                {t("common.reload", "Yenile")}
-              </button>
-            )}
-            <button
-              className="rounded-lg bg-gray-500 px-4 py-2 text-white"
-              onClick={() => close()}
-            >
-              {t("common.close", "Kapat")}
-            </button>
-          </div>
-        )}
-      </div>
     </>
   );
 };
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import react from "@vitejs/plugin-react";
 
 import * as path from "path";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+//import { VitePWA } from "vite-plugin-pwa";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
@@ -19,39 +19,6 @@ export default defineConfig({
     },
     extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
-  plugins: [
-    react(),
-    svgr(),
-    VitePWA({
-      registerType: "prompt",
-      manifest: {
-        name: "egaranti",
-        short_name: "egaranti",
-      },
-      workbox: {
-        maximumFileSizeToCacheInBytes: 3000000,
-        cleanupOutdatedCaches: true,
-        skipWaiting: true,
-        clientsClaim: true,
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/.*\.(png|jpg|jpeg|svg|gif)$/,
-            handler: "CacheFirst",
-            options: {
-              cacheName: "images",
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-              },
-            },
-          },
-          {
-            urlPattern: /./,
-            handler: "NetworkOnly",
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react(), svgr()],
   base: "/",
 });
