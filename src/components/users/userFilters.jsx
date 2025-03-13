804387f1ca60ef7b762444f692a1931637ffab40
Umut Keskin <ukeskin2626@gmail.com>
2025-03-13 12:03:15 +0300

    hide searchs

--- src/components/users/userFilters.jsx
+++ src/components/users/userFilters.jsx
@@ -8,13 +8,13 @@ const UserFilters = ({ filters, setFilters }) => {
   return (
     <div className="mb-6 flex gap-4">
       <div className="relative flex-1">
-        <Search className="absolute left-3 top-4 h-4 w-4 text-gray-400" />
+        {/* <Search className="absolute left-3 top-4 h-4 w-4 text-gray-400" />
         <Input
           placeholder="İsim veya email ile ara..."
           value={filters.search}
           onChange={(e) => setFilters({ ...filters, search: e.target.value })}
           className="max-w-xs pl-9"
-        />
+        /> */}
       </div>
     </div>
   );
