d7ce6150a67db5cdf7140898ece663ace2c62f4a
Umut Keskin <ukeskin2626@gmail.com>
2025-03-14 12:06:33 +0300

    last

--- src/components/requests/requestDetail/components/followUpDataPreview.jsx
+++ src/components/requests/requestDetail/components/followUpDataPreview.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { SquareChartGantt } from "lucide-react";
+
 const FollowUpDataPreview = ({
   followupData,
   request,
@@ -11,6 +13,11 @@ const FollowUpDataPreview = ({
     <div className="mb-6 rounded-lg border border-blue-200 bg-blue-100 p-4">
       <div className="mb-3 flex items-center justify-between">
         <h3 className="font-medium text-gray-900">İşlem Bilgileri</h3>
+        <div className="flex h-8 w-8 items-center justify-center rounded-full border border-blue-400 bg-blue-500">
+          <SquareChartGantt className="h-5 w-5 text-gray-200" />
+        </div>
+      </div>
+      <div className="mb-3 flex items-center justify-between">
         {request.followupDemandData && (
           <button
             onClick={() => setFollowUpDialogOpen(true)}
@@ -28,14 +35,14 @@ const FollowUpDataPreview = ({
             </span>
             <span className="text-sm text-gray-600">
               {(field.value ?? field.spareParts) ? (
-                  Array.isArray(field.value ?? field.spareParts) ? (
-                    (field.value ?? field.spareParts).join(", ")
-                  ) : (
-                    (field.value ?? field.spareParts).toString()
-                  )
+                Array.isArray(field.value ?? field.spareParts) ? (
+                  (field.value ?? field.spareParts).join(", ")
                 ) : (
-                  <span className="text-gray-400">Belirtilmemiş</span>
-                )}
+                  (field.value ?? field.spareParts).toString()
+                )
+              ) : (
+                <span className="text-gray-400">Belirtilmemiş</span>
+              )}
             </span>
           </div>
         ))}
--- src/components/requests/requestDetail/components/requestDetailContent.jsx
+++ src/components/requests/requestDetail/components/requestDetailContent.jsx
@@ -4,6 +4,8 @@ import FollowUpDataPreview from "./followUpDataPreview";
 
 import DynamicForm from "@/components/forms/dynamicForm";
 
+import { Handshake } from "lucide-react";
+
 const RequestDetailContent = ({
   request,
   isEditing,
@@ -46,6 +48,14 @@ const RequestDetailContent = ({
               setFollowUpDialogOpen={setFollowUpDialogOpen}
             />
           )}
+        <div className="mb-6 rounded-lg border border-green-200 bg-green-100 p-4">
+          <div className="mb-3 flex items-center justify-between">
+            <h3 className="font-medium text-gray-900">Hakediş Değerleri</h3>
+            <div className="flex h-8 w-8 items-center justify-center rounded-full border border-green-500 bg-green-500">
+              <Handshake className="h-5 w-5 text-gray-200" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
